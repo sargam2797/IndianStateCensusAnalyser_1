12a56759a990c5d80064166b09b8e8d218d0262b
Sargam <sargampandey27oct@gmail.com>
2019-12-09 16:07:56 +0530

    added: 'read csv file by list' method instead of iterator

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -5,6 +5,7 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.List;
 import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
@@ -12,9 +13,9 @@ public class CensusAnalyser {
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
             ICSvBuilder csvBuilder = CSVBuilderFactory.createCsvBuilder();
-            Iterator<IndiaCensusCSV> censusCSVIterator = csvBuilder.loadParticularCsvFile(reader,
+            List<IndiaCensusCSV> censusCSVList = csvBuilder.loadParticularCsvFileByList(reader,
                     IndiaCensusCSV.class);
-            return (this.getCount(censusCSVIterator));
+            return censusCSVList.size();
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
