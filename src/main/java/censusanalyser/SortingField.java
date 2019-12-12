338b8de163743704a3329676cc62e17d427c4d9e
Sargam <sargampandey27oct@gmail.com>
2019-12-12 09:41:48 +0530

    refactored: parameter and map object names

--- src/main/java/censusanalyser/SortingField.java
+++ src/main/java/censusanalyser/SortingField.java
@@ -5,22 +5,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SortingField {
-    static Map<SortingField.Parameter, Comparator> sortParameterComparator = new HashMap<>();
+    static Map<Parameter, Comparator> sortByFields = new HashMap<>();
 
     enum Parameter {
         STATE, POPULATION, AREA, DENSITY;
     }
 
-    public static Comparator getParameter(SortingField.Parameter parameter) {
-        sortParameterComparator.put(Parameter.DENSITY, Comparator.<IndiaCensusDAO,Integer>comparing(census ->
+    public static Comparator getParameter(Parameter fields) {
+        sortByFields.put(Parameter.DENSITY, Comparator.<IndiaCensusDAO,Integer>comparing(census ->
                 census.densityPerSqKm));
-        sortParameterComparator.put(Parameter.STATE, Comparator.<IndiaCensusDAO,String>comparing(census ->
+        sortByFields.put(Parameter.STATE, Comparator.<IndiaCensusDAO,String>comparing(census ->
                 census.state)) ;
-        sortParameterComparator.put(Parameter.POPULATION, Comparator.<IndiaCensusDAO,Integer>comparing(census ->
+        sortByFields.put(Parameter.POPULATION, Comparator.<IndiaCensusDAO,Integer>comparing(census ->
                 census.population));
-        sortParameterComparator.put(Parameter.AREA, Comparator.<IndiaCensusDAO,Integer>comparing(census ->
+        sortByFields.put(Parameter.AREA, Comparator.<IndiaCensusDAO,Integer>comparing(census ->
                 census.areaInSqKm));
-        Comparator<IndiaCensusDAO> comparator = sortParameterComparator.get(parameter);
+        Comparator<IndiaCensusDAO> comparator = sortByFields.get(fields);
         return comparator;
     }
 
